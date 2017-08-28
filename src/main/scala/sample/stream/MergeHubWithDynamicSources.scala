7fab4dc1d5f9fed75e9fe406950d7c7aca48fc44
Bernet Paul <Paul.Bernet@finnova.com>
2017-08-28 12:18:29 +0200

    Add doc clarifications

--- src/main/scala/sample/stream/MergeHubWithDynamicSources.scala
+++ src/main/scala/sample/stream/MergeHubWithDynamicSources.scala
@@ -1,7 +1,7 @@
 package sample.stream
 
 import akka.NotUsed
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, Cancellable}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{MergeHub, RunnableGraph, Sink, Source}
 
@@ -9,7 +9,9 @@ import scala.concurrent.duration._
 
 /**
   * Example taken from Doc:
-  * http://doc.akka.io/docs/akka/current/scala/stream/stream-dynamic.html#dynamic-fan-in-and-fan-out-with-mergehub-and-broadcasthub
+  * http://doc.akka.io/docs/akka/current/scala/stream/stream-dynamic.html#dynamic-fan-in-and-fan-out-with-mergehub-broadcasthub-and-partitionhub
+  *
+  * The same principle for MergeHub.source can be found in WebsocketChatEcho
   */
 object MergeHubWithDynamicSources {
   implicit val system = ActorSystem()
@@ -19,26 +21,23 @@ object MergeHubWithDynamicSources {
 
     val consumer = Sink.foreach(println)
 
-    // Attach a MergeHub Source to the consumer. This will materialize to a
-    // corresponding Sink.
-    val runnableGraph: RunnableGraph[Sink[String, NotUsed]] =
-    MergeHub.source[String](perProducerBufferSize = 16).to(consumer)
+    // Attach a MergeHub Source to the consumer. This will materialize to a corresponding Sink.
+    val runnableGraph: RunnableGraph[Sink[String, NotUsed]] = MergeHub.source[String](perProducerBufferSize = 16).to(consumer)
 
     // By running/materializing the consumer we get back a Sink, and hence
-    // now have access to feed elements into it. This Sink can be materialized
-    // any number of times, and every element that enters the Sink will
+    // now have access to feed elements into it.
+    // This Sink can be materialized any number of times, and every element that enters the Sink will
     // be consumed by our consumer.
     val toConsumer: Sink[String, NotUsed] = runnableGraph.run()
 
-    // Feeding two independent sources into the hub.
+    // Feed two independent sources into the hub.
     Source.single("Hello!").runWith(toConsumer)
     Source.single("Hub!").runWith(toConsumer)
 
-    // Add another source and materialize it two times
-    val tickSource = Source.tick(1.seconds, 1.second, 1)
+    // Add another source and materialize it
+    val tickSource: Source[String, Cancellable] = Source.tick(1.seconds, 1.second, 1)
       .scan(0)(_ + _)
       .map(_.toString)
-    tickSource.runWith(toConsumer)
     tickSource.runWith(toConsumer)
   }
 }
