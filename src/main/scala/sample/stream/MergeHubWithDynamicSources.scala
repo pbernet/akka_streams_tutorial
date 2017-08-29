07a839c6f12d1775ff3578f6bd3c80f13d88637a
Bernet Paul <Paul.Bernet@finnova.com>
2017-08-29 15:17:25 +0200

    Change comment

--- src/main/scala/sample/stream/MergeHubWithDynamicSources.scala
+++ src/main/scala/sample/stream/MergeHubWithDynamicSources.scala
@@ -12,6 +12,7 @@ import scala.concurrent.duration._
   * http://doc.akka.io/docs/akka/current/scala/stream/stream-dynamic.html#dynamic-fan-in-and-fan-out-with-mergehub-broadcasthub-and-partitionhub
   *
   * The same principle for MergeHub.source can be found in WebsocketChatEcho
+  * There it could be used as a "reverse proxy"
   */
 object MergeHubWithDynamicSources {
   implicit val system = ActorSystem()
@@ -21,13 +22,11 @@ object MergeHubWithDynamicSources {
 
     val consumer = Sink.foreach(println)
 
-    // Attach a MergeHub Source to the consumer. This will materialize to a corresponding Sink.
+    // Attach a MergeHub Source to the consumer. This will materialize to a corresponding Sink
     val runnableGraph: RunnableGraph[Sink[String, NotUsed]] = MergeHub.source[String](perProducerBufferSize = 16).to(consumer)
 
-    // By running/materializing the consumer we get back a Sink, and hence
-    // now have access to feed elements into it.
-    // This Sink can be materialized any number of times, and every element that enters the Sink will
-    // be consumed by our consumer.
+    // By running/materializing the graph we get back a Sink, and hence now have access to feed elements into it
+    // This Sink can then be materialized any number of times, and every element that enters the Sink will be consumed by our consumer
     val toConsumer: Sink[String, NotUsed] = runnableGraph.run()
 
     // Feed two dynamic fan-in sources into the hub
