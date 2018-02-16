118acfd90c84adc146622a13ad16f45d0bcb77a5
pbernet <paul.bernet@gmail.com>
2018-02-16 16:27:23 +0100

    Add better comments

--- src/main/scala/kafka/WordCountProducer.scala
+++ src/main/scala/kafka/WordCountProducer.scala
@@ -29,6 +29,8 @@ object WordCountProducer extends App {
 
   val bootstrapServers = "localhost:9092"
 
+  val topic = "wordcount-input"
+
   //initial msg in topic, required to create the topic before any consumer subscribes to it
   val InitialMsg = "truth"
 
@@ -70,18 +72,18 @@ object WordCountProducer extends App {
     system.terminate()
   }
 
-  initializeTopic("wordcount-input")
+  initializeTopic(topic)
   val randomMap: Map[Int, String] = TextMessageGenerator.genRandTextWithKeyword(1000,1000, 3, 5, 5, 10, "fakeNews").split("([!?.])").toList.zipWithIndex.toMap.map(_.swap)
-  val doneFuture = produce("wordcount-input", randomMap)
+  val doneFuture = produce(topic, randomMap)
 
   doneFuture.recover{
     case e: NetworkException => {
       println(s"NetworkException $e occurred - Retry...")
-      produce("wordcount-input", randomMap)
+      produce(topic, randomMap)
     }
     case e: UnknownTopicOrPartitionException => {
       println(s"UnknownTopicOrPartitionException $e occurred - Retry...")
-      produce("wordcount-input", randomMap)
+      produce(topic, randomMap)
     }
     case ex: RuntimeException => {
       println(s"RuntimeException $ex occurred - Do not retry. Shutdown...")
@@ -93,31 +95,32 @@ object WordCountProducer extends App {
 /**
   * Example of a CustomPartitioner hook in the producer realm
   * Done like this, because here we have access to cluster.availablePartitionsForTopic
-  * The partitioning here gives no added value for the downstream WordCountKStreams nor WordCountConsumer,
-  * because the whole message is put into the partition and WordCountKStreams counts words
+  *
+  * The partitioning here gives no added value for the downstream WordCountKStreams,
+  * because here a whole message is put into the partition. WordCountKStreams counts words though
   *
   */
 class CustomPartitioner extends Partitioner {
   override def partition(topic: String, key: Any, keyBytes: Array[Byte], value: Any, valueBytes: Array[Byte], cluster: Cluster): Int = {
     val partitionInfoList: util.List[PartitionInfo] = cluster.availablePartitionsForTopic(topic)
-    val partitionCount: Int = partitionInfoList.size
-    val fakeNewsPartition: Int = 0
+    val partitionCount = partitionInfoList.size
+    val fakeNewsPartition = 0
 
-    //System.out.println("Partitioner received key: " + key + " and value: " + value);
+    //println("CustomPartitioner received key: " + key + " and value: " + value)
 
     if (value.toString.contains("fakeNews")) {
-      System.out.println("Send message: " + value + "   to fakeNewsPartition")
+      println("CustomPartitioner send message: " + value + " to fakeNewsPartition")
       fakeNewsPartition
     }
     else ThreadLocalRandom.current.nextInt(1, partitionCount) //round robin
   }
 
   override def close(): Unit = {
-    System.out.println("Partitioner: " + Thread.currentThread + " received close")
+    println("CustomPartitioner: " + Thread.currentThread + " received close")
   }
 
   override def configure(configs: util.Map[String, _]): Unit = {
-    System.out.println("Partitioner received configure with configuration: " + configs)
+    println("CustomPartitioner received configure with configuration: " + configs)
   }
 }
 
