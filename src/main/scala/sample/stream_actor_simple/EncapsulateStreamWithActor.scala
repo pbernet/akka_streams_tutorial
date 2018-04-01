ef1f0bbf3a5bd7d07d1de2e816708227c2dc4482
pbernet <paul.bernet@gmail.com>
2018-04-01 23:00:02 +0200

    Rename to be in sync with inspiration blog

--- src/main/scala/sample/stream_actor_simple/EncapsulateStreamWithActor.scala
+++ src/main/scala/sample/stream_actor_simple/EncapsulateStreamWithActor.scala
@@ -2,9 +2,15 @@ package sample.stream_actor_simple
 
 import akka.actor.{ActorSystem, Props}
 import akka.stream.ActorMaterializer
+
 import scala.concurrent.duration._
 
-object LessTrivialExample extends App {
+/**
+  * Inspired by:
+  * http://blog.colinbreck.com/integrating-akka-streams-and-akka-actors-part-ii
+  *
+  */
+object EncapsulateStreamWithActor extends App {
   implicit val system = ActorSystem()
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
