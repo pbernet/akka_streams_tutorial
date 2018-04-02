6c49fb57a8a0c4b97bbd2861fb995c91f33afb3a
pbernet <paul.bernet@gmail.com>
2018-04-02 10:47:33 +0200

    Add partitionToFastestConsumer function

--- src/main/scala/sample/stream/PartitionHubWithDynamicSinks.scala
+++ src/main/scala/sample/stream/PartitionHubWithDynamicSinks.scala
@@ -2,8 +2,8 @@ package sample.stream
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Keep, PartitionHub, RunnableGraph, Source}
+import akka.stream.{ActorMaterializer, ThrottleMode}
 
 import scala.concurrent.duration._
 
@@ -11,21 +11,22 @@ import scala.concurrent.duration._
   * Inspired by:
   * http://doc.akka.io/docs/akka/2.5/scala/stream/stream-dynamic.html#using-the-partitionhub
   *
-  * Keypoints:
-  * - The selection of consumers is done with the roundRobin function
-  * - Each element can be routed to only one consumer
+  * Partitioning functions:
+  * - partitionRoundRobin: Route to only one consumer (incl. slow consumers)
+  * - partitionToFastestConsumer: Route to the fastest consumer
   *
   */
 object PartitionHubWithDynamicSinks {
   implicit val system = ActorSystem()
+  implicit val ec = system.dispatcher
   implicit val materializer = ActorMaterializer()
 
   def main(args: Array[String]): Unit = {
 
-    val producer = Source.tick(1.second, 1.second, "message").zipWith(Source(1 to 10))((a, b) => s"$a-$b")
+    val producer = Source.tick(1.second, 100.millis, "message").zipWith(Source(1 to 100))((a, b) => s"$a-$b")
 
-    // New instance of the partitioner function and its state is created for each materialization of the PartitionHub
-    def roundRobin(): (PartitionHub.ConsumerInfo, String) => Long = {
+    // A new instance of the partitioner functions and its state is created for each materialization of the PartitionHub
+    def partitionRoundRobin(): (PartitionHub.ConsumerInfo, String) => Long = {
       var i = -1L
 
       (info, elem) => {
@@ -34,20 +35,28 @@ object PartitionHubWithDynamicSinks {
       }
     }
 
+    def partitionToFastestConsumer(): (PartitionHub.ConsumerInfo, String) => Long = {
+      (info: PartitionHub.ConsumerInfo, each:String) => info.consumerIds.minBy(id => info.queueSize(id))
+    }
+
     // Attach a PartitionHub Sink to the producer. This will materialize to a corresponding Source
     // We need to use toMat and Keep.right since by default the materialized value to the left is used
     val runnableGraph: RunnableGraph[Source[String, NotUsed]] =
     producer.toMat(PartitionHub.statefulSink(
-      () => roundRobin(),
-      startAfterNrOfConsumers = 2, bufferSize = 256))(Keep.right)
+      //Switch the partitioning function
+      () => partitionRoundRobin(),
+      //() => partitionToFastestConsumer(),
+      startAfterNrOfConsumers = 1, bufferSize = 1))(Keep.right)
 
     // By running/materializing the producer, we get back a Source, which
     // gives us access to the elements published by the producer.
     val fromProducer: Source[String, NotUsed] = runnableGraph.run()
 
-    // Feed three dynamic fan-out sinks into the Hub
-    fromProducer.runForeach(msg => println("consumer1: " + msg))
-    fromProducer.runForeach(msg => println("consumer2: " + msg))
-    fromProducer.runForeach(msg => println("consumer3: " + msg))
+    // Attach three dynamic fan-out sinks to the PartitionHub
+    fromProducer.runForeach(msg => println("fast consumer1 received: " + msg))
+    fromProducer.throttle(100, 1.millis, 10, ThrottleMode.Shaping)
+      .runForeach(msg => println("slow consumer2 received: " + msg))
+    fromProducer.throttle(100, 2.millis, 10, ThrottleMode.Shaping)
+      .runForeach(msg => println("really slow consumer3 received: " + msg))
   }
 }
