c56f3d253e7c2df91c1c5014c722821913ed752c
pbernet <paul.bernet@elca.ch>
2018-08-06 16:59:08 +0200

    Improve Logging config

--- src/main/scala/sample/stream_divert/AlsoToExample.scala
+++ src/main/scala/sample/stream_divert/AlsoToExample.scala
@@ -6,15 +6,11 @@ import akka.stream.scaladsl.{Flow, Sink, Source}
 import akka.stream.{ActorMaterializer, Attributes}
 
 /**
-  * Example with alsoTo and akka streams logging
+  * Example with alsoTo and akka streams logging (via slf4j and logback)
   *
   * Inspired by:
   * https://blog.softwaremill.com/akka-streams-pitfalls-to-avoid-part-2-f93e60746c58
   * https://doc.akka.io/docs/akka/2.5/stream/stream-cookbook.html?language=scala#logging-in-streams
-  *
-  * Issues:
-  * * how can the customLogger be configured in application.conf?
-  * * how does logging work in the alsoTo path?
   *
   */
 
@@ -22,11 +18,15 @@ object AlsoToExample {
   implicit val system = ActorSystem("AlsoToExample")
   implicit val executionContext = system.dispatcher
   implicit val materializerServer = ActorMaterializer()
-  //implicit val adapter = Logging(system, "customLogger")
+  implicit val adapter = Logging(system, "MyLogger")
 
   def main(args: Array[String]) {
     val source = Source(1 to 10)
-    val sink = Sink.foreach{x: Int => println(s"elem: $x to sink")}
+    val sink = Sink.foreach{x: Int => adapter.log(Logging.InfoLevel, s" --> elem: $x logged in sink")}
+    val sinkSlow = Sink.foreach { x: Int =>
+      Thread.sleep(1000)
+      adapter.log(Logging.InfoLevel, s" --> elem: $x logged in alsoTo sinkSlow")
+    }
 
     val flow = Flow[Int]
       .log("before alsoTo")
@@ -36,11 +36,7 @@ object AlsoToExample {
         onFinish = Logging.InfoLevel,
         onFailure = Logging.DebugLevel
       ))
-      .alsoTo(Sink.foreach { x: Int =>
-        Thread.sleep(100)
-        println(s" elem: $x alsoTo sink")
-      }
-      )
+      .alsoTo(sinkSlow)
       .log("after alsoTo")
       .withAttributes(
         Attributes.logLevels(
