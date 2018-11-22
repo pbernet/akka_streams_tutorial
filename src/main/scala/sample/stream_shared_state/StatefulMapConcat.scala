3c88293907c919d7dfdb076cc43b13ba3b33f828
pbernet <paul.bernet@elca.ch>
2018-11-22 13:59:24 +0100

    Adapt to serve as file cache

--- src/main/scala/sample/stream_shared_state/StatefulMapConcat.scala
+++ src/main/scala/sample/stream_shared_state/StatefulMapConcat.scala
@@ -1,5 +1,8 @@
 package sample.stream_shared_state
 
+import java.io.File
+import java.nio.file.Files
+
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Sink, Source}
@@ -15,6 +18,7 @@ import scala.util.Try
   * Stateful stream processing inspired by:
   * https://stackoverflow.com/questions/37902354/akka-streams-state-in-a-flow
   *
+  * Adapted to serve as file cache
   */
 object StatefulMapConcat {
 
@@ -23,22 +27,21 @@ object StatefulMapConcat {
     implicit val materializer = ActorMaterializer()
 
     //encapsulating your input
-    case class IdentValue(id: Int, value: String)
+    case class IdentValue(id: Int, file: File)
 
     //some random generated input
-    val identValues = List.fill(20)(IdentValue(abs(nextInt()) % 5, "valueHere"))
+    val identValues = List.fill(20)(IdentValue(abs(nextInt()) % 5, null))
 
     val stateFlow = Flow[IdentValue].statefulMapConcat{ () =>
       //state with already processed ids
-      var ids = Set.empty[Int]
-      identValue => if (ids.contains(identValue.id)) {
+      var stateMap = Map.empty[Int, File]
+      identValue => if (stateMap.contains(identValue.id)) {
         //Do nothing
-        println(identValue.value)
         List(identValue)
       } else {
-        //Do sth eg download element
-        println(identValue)
-        ids = ids + identValue.id
+        //Do sth eg download a file and element and add to Map
+        stateMap = stateMap + (identValue.id -> Files.createTempFile(identValue.id.toString, ".txt").toFile)
+        println("New state: " + stateMap)
         List(identValue)
       }
     }
