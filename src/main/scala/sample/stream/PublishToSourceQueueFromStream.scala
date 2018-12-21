019e2bb8b05946bff43bbd686d3083c5fecc5cc4
pbernet <paul.bernet@gmail.com>
2018-12-21 21:40:35 +0100

    Change comment

--- src/main/scala/sample/stream/PublishToSourceQueueFromStream.scala
+++ src/main/scala/sample/stream/PublishToSourceQueueFromStream.scala
@@ -1,20 +1,21 @@
 package sample.stream
 
-import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.scaladsl.{Flow, Sink, Source, SourceQueueWithComplete}
 import akka.stream.{ActorMaterializer, DelayOverflowStrategy, OverflowStrategy, QueueOfferResult}
+import akka.{Done, NotUsed}
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
+import scala.util.{Failure, Success}
 
 /**
   * Doc:
   * https://doc.akka.io/docs/akka/2.5/stream/stream-integrations.html?language=scala#source-queue
   *
-  * Observation:
-  *  * The buffer size must be large enough (eg 1000) to allow for massive parallelism in mapAsync (eg 100)
-  *  * If buffer size is 1 then the mapAsync can only be run with 1
-  *  * With buffer size 1 and mapAsync 100 the queue stops accepting after element 173
+  * Observations:
+  *  * The bufferSize should be large enough
+  *  * With bufferSize = 1, the parallelism can not be > 1 - queue stops accepting elements with failures (on my machine after 173)
   *
   *  Similar example: MergeHubWithDynamicSources
   */
@@ -26,7 +27,8 @@ object PublishToSourceQueueFromStream {
 
   def main(args: Array[String]): Unit = {
 
-    val bufferSize = 1000
+    val bufferSize = 100
+    val parallelism = 1
 
     val slowSink: Sink[Seq[Int], NotUsed] =
       Flow[Seq[Int]]
@@ -39,14 +41,29 @@ object PublishToSourceQueueFromStream {
       .to(slowSink)
       .run
 
-    Source(1 to 1000)
-      .mapAsync(100)(x ⇒ {
-      queue.offer(x).map {
-        case QueueOfferResult.Enqueued    ⇒ println(s"enqueued $x")
-        case QueueOfferResult.Dropped     ⇒ println(s"dropped $x")
-        case QueueOfferResult.Failure(ex) ⇒ println(s"Offer failed ${ex.getMessage}")
-        case QueueOfferResult.QueueClosed ⇒ println("Source Queue closed")
+
+    val doneConsuming: Future[Done] = queue.watchCompletion()
+    signalWhen(doneConsuming, "consuming") //never completes...
+
+    val donePublishing: Future[Done] = Source(1 to 1000)
+      .mapAsync(parallelism){each => {
+      queue.offer(each).map {
+        case QueueOfferResult.Enqueued    => println(s"enqueued $each")
+        case QueueOfferResult.Dropped     => println(s"dropped $each")
+        case QueueOfferResult.Failure(ex) => println(s"Offer failed ${ex.getMessage}")
+        case QueueOfferResult.QueueClosed => println("Source Queue closed")
       }
-    }).runWith(Sink.ignore)
+    }}.runWith(Sink.ignore)
+    signalWhen(donePublishing, "publishing")
+  }
+
+  def signalWhen(done: Future[Done], operation: String) = {
+    done.onComplete {
+      case Success(b) =>
+        println(s"Finished: $operation")
+      case Failure(e) =>
+        println(s"Failure: ${e.getMessage}. About to terminate...")
+        system.terminate()
+    }
   }
 }
