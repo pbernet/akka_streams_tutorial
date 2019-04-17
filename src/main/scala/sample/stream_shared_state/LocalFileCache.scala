72151cf83b26f7a37a3b5edc58a9a41a6dd77e77
pbernet <paul.bernet@elca.ch>
2019-04-17 15:57:48 +0200

    Try to add downloadInProgress barrier and document the non-parallelism

--- src/main/scala/sample/stream_shared_state/LocalFileCache.scala
+++ src/main/scala/sample/stream_shared_state/LocalFileCache.scala
@@ -2,7 +2,7 @@ package sample.stream_shared_state
 
 import java.io.File
 import java.net.URI
-import java.nio.file.Paths
+import java.nio.file.{Path, Paths}
 import java.util.concurrent.ThreadLocalRandom
 
 import akka.actor.ActorSystem
@@ -12,7 +12,6 @@ import org.apache.commons.io.FileUtils
 import org.slf4j.{Logger, LoggerFactory}
 
 import scala.collection.immutable
-import scala.collection.immutable.ListMap
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.util.control.NonFatal
@@ -34,8 +33,11 @@ import scala.util.control.NonFatal
   * Simulate download exceptions and resume stream: Supervision.Resume keeps the state
   *
   * TODO
-  * If used in a ProcessingApp, where faulty elements are put in error queue and thus need to remain in file cache
-  * Use File.setLastModified to extend the keeping period
+  * Since the http download is executed from the statefulMapConcat stage, the http download can not occur in parallel
+  *
+  * If used in a ProcessingApp, where faulty elements could occur at a later stage and put in an error queue
+  * and thus the original downloaded file needs to remain in file cache for later reference
+  * Simulate File.setLastModified to extend the keeping period
   *
   * Multiple logback.xml are on the classpath, read from THIS on in project
   *
@@ -66,62 +68,88 @@ object LocalFileCache {
     case class Message(id: Int, file: File)
 
     //Generate random messages with IDs between 0/10
-    val messages = List.fill(100)(Message(ThreadLocalRandom.current.nextInt(0, 10 * scaleFactor), null))
+    val messages = List.fill(1000)(Message(ThreadLocalRandom.current.nextInt(0, 10 * scaleFactor), null))
 
 
     val stateFlow = Flow[Message].statefulMapConcat { () =>
+      //We are in a thread safe env, like in an Actor
 
-      //State with already processed IDs
-      var stateMap = ListMap.empty[Int, File]
+      //These internal states are kept by akka streams on Supervision.Resume
+      var downloaded = scala.collection.mutable.ListMap.empty[Int, Path]
+      var downloadInProgress = scala.collection.mutable.ListBuffer.empty[Int]
 
-      logger.info(s"About to load file refs from: $localFileCache...")
-      val loadedResults =  new FileLister().run(localFileCache.toFile)
+      val loadedResults = new FileLister().run(localFileCache.toFile)
+      logger.info(s"Loaded ${loadedResults.size} file refs from: $localFileCache...")
       loadedResults.forEach { each: File =>
         logger.debug(s"Add file: ${each.getName} with lastModified: ${each.lastModified()}")
-        stateMap = stateMap + (each.getName.dropRight(4).toInt -> each)
+        downloaded += (each.getName.dropRight(4).toInt -> each.toPath)
       }
 
       logger.info(s"Finished loading: ${loadedResults.size} file refs. Start processing loop...")
 
       def evictOldestFile() = {
-        if (stateMap.nonEmpty) {
-          val timeStampOfOldestFile = stateMap.values.head.lastModified()
-          val oldestFileID = stateMap.keys.head
+        logger.debug(s"Eviction started on $downloaded")
+        if (downloaded.nonEmpty) {
+          val timeStampOfOldestFile = downloaded.values.head.toFile.lastModified()
+          val oldestFileID = downloaded.keys.head
           logger.debug(s"About to evict oldest cached file with ID: $oldestFileID")
           if (timeStampOfOldestFile + evictionTime < System.currentTimeMillis()) {
             //TODO make more robust, filesystem and memory should not get out of sync
-            stateMap = stateMap - oldestFileID
+            if (downloaded.nonEmpty){
+            downloaded -= oldestFileID
             FileUtils.deleteQuietly(localFileCache.resolve(Paths.get(oldestFileID.toString + ".zip")).toFile)
-            logger.info(s"REMOVED oldest file: $oldestFileID after: $evictionTime ms. Remaining files: ${stateMap.keys.size}")
+            logger.info(s"REMOVED oldest file: $oldestFileID after: $evictionTime ms. Remaining files: ${downloaded.keys.size}")
+            }
           }
         }
       }
 
-      def processNext(message: Message) = {
-        if (stateMap.contains(message.id)) {
+      def processNext(message: Message): List[Message] = {
+        if (downloaded.contains(message.id)) {
           logger.info("Cache hit for ID: " + message.id)
           List(message)
         } else {
           logger.info("Cache miss - download for ID: " + message.id)
 
-          val destinationFile = localFileCache.resolve(Paths.get(message.id.toString + ".zip"))
-          val url = new URI("http://127.0.0.1:6001/download/" + message.id.toString)
-          val downloadedFile = new DownloaderRetry().download(url, destinationFile)
+          if (downloadInProgress.contains(message.id)) {
+            logger.info(s"Download is in progress for ID: ${message.id}. Retry after 500 ms. All ID: $downloadInProgress")
+            Thread.sleep(500)
+            processNext(message)
+          }
 
-          stateMap = stateMap + (message.id -> downloadedFile.toFile)
+          var downloadedFile: Path = null
+          try {
+            downloadInProgress += message.id
+            val destinationFile = localFileCache.resolve(Paths.get(message.id.toString + ".zip"))
+            val url = new URI("http://127.0.0.1:6001/downloadflaky/" + message.id.toString)
+            downloadedFile = new DownloaderRetry().download(url, destinationFile)
+            downloaded += (message.id -> downloadedFile)
+          } catch {
+            case e@(_: Throwable) => {
+              logger.error("Ex during download. " + e)
+
+              throw e
+            }
+          } finally {
+            //we need to remove in any case, otherwise we produce a endless loop
+            downloadInProgress -= message.id
+          }
           List(message)
         }
       }
 
       message =>
+        logger.debug(s"Evicting...")
         evictOldestFile()
+        logger.debug(s"Start processing for: ${message.id}")
         processNext(message)
 
     }
 
     Source(messages)
-      .throttle(2 * scaleFactor, 1.second, 2 * scaleFactor, ThrottleMode.shaping)
+      .throttle(10 * scaleFactor, 1.second, 2 * scaleFactor, ThrottleMode.shaping)
       .via(stateFlow)
+      //TODO Simulate error in a later processing stage
       .withAttributes(ActorAttributes.supervisionStrategy(deciderFlow))
       .runWith(Sink.seq)
       .onComplete {
