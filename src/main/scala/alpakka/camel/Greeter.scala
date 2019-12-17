76aba159b2c594e9b9b1bb99dcd8833b1bbb7991
pbernet <paul.bernet@elca.ch>
2019-12-17 13:12:43 +0100

    Change to 0.0.0.0

--- src/main/scala/alpakka/camel/Greeter.scala
+++ src/main/scala/alpakka/camel/Greeter.scala
@@ -14,13 +14,13 @@ object Greeter extends App {
   implicit val system = ActorSystem("Greeter")
   implicit val streamContext = StreamContext()
 
-  // TCP greeter service. Use with e.g. "telnet localhost 5150"
-  receiveRequestBody[String, String]("netty4:tcp://localhost:5150?textline=true")
+  // TCP greeter service. Use with e.g. telnet 0.0.0.0 5150
+  receiveRequestBody[String, String]("netty4:tcp://0.0.0.0:5150?textline=true")
     .map(s => s"hello $s")
     .reply.run()
 
-  // HTTP greeter service. Use with e.g. "curl http://localhost:8080/greeter?name=..."
-  receiveRequest[String, String]("jetty:http://localhost:8080/greeter")
+  // HTTP greeter service. Use with e.g. curl "http://0.0.0.0:8080/greeter?name=abc"
+  receiveRequest[String, String]("jetty:http://0.0.0.0:8080/greeter")
     .map(msg => StreamMessage(s"Hello ${msg.headers.getOrElse("name", "unknown")}\n"))
     .reply.run()
 }
