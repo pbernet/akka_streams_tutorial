b84443ee0cbb307e26174229cb42feea15b44d89
pbernet <paul.bernet@gmail.com>
2020-01-31 10:05:35 +0100

    Add resource file and comment

--- src/main/scala/alpakka/xml/XmlProcessing.scala
+++ src/main/scala/alpakka/xml/XmlProcessing.scala
@@ -17,10 +17,10 @@ import scala.util.Try
   * Parse XML and extract embedded base64 application/pdf docs
   *
   * Remarks:
-  * - Extraction of the file name is missing,
-  *   all the files are saved under the same dummy name
-  * - State case class could be used inside statefulMapConcat
-  * - Results could be written to fileSink (FileIO.toPath)
+  * - All result files are saved with the same name testfile_result.jpg
+  * - A case class could be used inside statefulMapConcat, to keep the state
+  * - Results could be written to a fileSink (FileIO.toPath), so this example
+  *   could be enhanced to a xml-to-file alpakka example. See [[FileIOEcho]]
   */
 
 object XmlProcessing {
@@ -28,7 +28,7 @@ object XmlProcessing {
   implicit val executionContext = system.dispatcher
 
   def main(args: Array[String]): Unit = {
-    val result: Future[immutable.Seq[String]] = FileIO.fromPath(Paths.get("./src/main/resources/largeEmbeddedPDF.xml"))
+    val result: Future[immutable.Seq[String]] = FileIO.fromPath(Paths.get("./src/main/resources/xml_with_base64_embedded.xml"))
       .via(XmlParsing.parser)
       .statefulMapConcat(() => {
         // state
@@ -41,7 +41,7 @@ object XmlProcessing {
               val mediaType = s.attributes.head._2
               println("Add mediaType: " + mediaType)
               immutable.Seq(mediaType)
-            case s: EndElement if s.localName == "observationMedia" =>
+            case s: EndElement if s.localName == "embeddedDoc" =>
               val text = textBuffer.toString
               println("Add payload: " + text)  //Note that large embedded PDFs are read into memory
               immutable.Seq(text)
@@ -60,7 +60,7 @@ object XmlProcessing {
           each: immutable.Seq[String] => {
             val dec1 = Base64.getMimeDecoder
             val decoded: Array[Byte] = dec1.decode(each.reverse.head)
-            val bos = new BufferedOutputStream(new FileOutputStream("test.pdf"))
+            val bos = new BufferedOutputStream(new FileOutputStream("testfile_result.jpg"))
             bos.write(decoded)
             bos.close()
           }
