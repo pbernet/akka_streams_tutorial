1e97688d1364c261549097c60d652a39e9dc57ac
pbernet <paul.bernet@elca.ch>
2020-05-07 17:09:03 +0200

    Add check to producer if Kafka is running. Send NACK to client if not.

--- src/main/scala/alpakka/tcp_to_websockets/hl7mllp/Hl7MllpListenerAkkaStreams.scala
+++ src/main/scala/alpakka/tcp_to_websockets/hl7mllp/Hl7MllpListenerAkkaStreams.scala
@@ -1,5 +1,8 @@
 package alpakka.tcp_to_websockets.hl7mllp
 
+import java.util.Properties
+
+import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.kafka.ProducerSettings
 import akka.stream.scaladsl.{Flow, Framing, Keep, Sink, Source, Tcp}
@@ -9,6 +12,7 @@ import ca.uhn.hl7v2.validation.impl.ValidationContextFactory
 import ca.uhn.hl7v2.{DefaultHapiContext, HL7Exception}
 import org.apache.commons.lang3.StringUtils
 import org.apache.commons.lang3.exception.ExceptionUtils
+import org.apache.kafka.clients.admin.AdminClient
 import org.apache.kafka.clients.producer.{Producer, ProducerRecord}
 import org.apache.kafka.common.serialization.StringSerializer
 import org.slf4j.{Logger, LoggerFactory}
@@ -37,10 +41,10 @@ import scala.util.{Failure, Success}
   * TODO Add HL7 over HTTP:
   * https://hapifhir.github.io/hapi-hl7v2/hapi-hl7overhttp/index.html
   *
-  * TODO Avoid loosing messages when Kafka is down
+  * TODO Avoid loosing messages in kafkaProducer when Kafka is down in a more graceful fashion
   * Switch to:
   * https://doc.akka.io/docs/alpakka-kafka/current/producer.html#producer-as-a-flow
-  * even better
+  * when it includes:
   * https://github.com/akka/alpakka-kafka/issues/1101
   *
   */
@@ -58,7 +62,7 @@ object Hl7MllpListenerAkkaStreams extends App {
   val producerSettings = ProducerSettings(system, new StringSerializer, new StringSerializer)
     .withBootstrapServers(bootstrapServers)
   val producer = initializeTopic(topic)
-
+  val adminClient = initializeAdminClient(bootstrapServers)
 
   //MLLP: messages begin after hex "0x0B" and continue until "0x1C|0x0D"
   val START_OF_BLOCK = "\u000b" //0x0B
@@ -67,7 +71,9 @@ object Hl7MllpListenerAkkaStreams extends App {
 
   val (address, port) = ("127.0.0.1", 6160)
   val serverBinding = server(system, address, port)
-  (1 to 1).par.foreach(each => localClient(each, 1000, system, address, port))
+
+  //(1 to 1).par.foreach(each => localStreamingClient(each, 1000, system, address, port))
+  localSingleMessageClient(1, 100, system, address, port)
 
 
   def server(system: ActorSystem, address: String, port: Int): Future[Tcp.ServerBinding] = {
@@ -86,54 +92,65 @@ object Hl7MllpListenerAkkaStreams extends App {
     val scrubber = Flow[String]
       .map(each => StringUtils.stripStart(each, START_OF_BLOCK))
 
-    val kafkaProducer = Flow[String]
+    val kafkaProducer: Flow[String, Either[Valid[String], Invalid[String]], NotUsed] = Flow[String]
       .map(each => {
-        producer.send(new ProducerRecord(topic, partition0, null: String, each))
-        each
+        val topicExists = adminClient.listTopics.names.get.stream.anyMatch((topicName: String) => topicName.equalsIgnoreCase(topic))
+        if (topicExists) {
+          producer.send(new ProducerRecord(topic, partition0, null: String, each))
+          Left(Valid(each))
+        } else {
+          logger.error("Topic " + topic + " does not exist or no connection to Kafka. Sending NACK to client")
+          Right(Invalid(each, Some(new RuntimeException("Kafka connection problem. Sending NACK to client"))))
+        }
       })
 
     // Parse the hairy HL7 message beast
-    val hl7Parser = Flow[String]
-      .map(each => {
-        val context = new DefaultHapiContext
+    val hl7Parser = Flow[Either[Valid[String], Invalid[String]]]
+      .map {
+        case left@Left(_) => {
+          val each = left.value.payload
 
-        // The ValidationContext is used during parsing as well as during
-        // validation using {@link ca.uhn.hl7v2.validation.Validator} objects.
+          val context = new DefaultHapiContext
 
-        // Set to false to do parsing without validation
-        context.getParserConfiguration.setValidating(false)
-        // Set to false, because there is currently no separate validation step
-        context.setValidationContext(ValidationContextFactory.noValidation)
+          // The ValidationContext is used during parsing as well as during
+          // validation using {@link ca.uhn.hl7v2.validation.Validator} objects.
 
-        val parser = context.getPipeParser
+          // Set to false to do parsing without validation
+          context.getParserConfiguration.setValidating(false)
+          // Set to false, because there is currently no separate validation step
+          context.setValidationContext(ValidationContextFactory.noValidation)
 
-        try {
-          logger.info("About to parse message: " + printableShort(each))
-          val message = parser.parse(each)
-          logger.info("Successfully parsed")
+          val parser = context.getPipeParser
 
-          val ack = parser.encode(message.generateACK())
-          encodeMllp(ack)
-        } catch {
-          case ex: HL7Exception =>
-            val rootCause = printable(ExceptionUtils.getRootCause(ex).getMessage)
-            logger.error(s"Error during parsing. Problem with message structure. Answer with NACK. Cause: $rootCause")
-            //TODO Build NACK from message. Note that there is a piggyback ex.getResponseMessage which could be used
-            val nack = "NACK"
-            encodeMllp(nack)
-          case ex: Throwable =>
-            val rootCause = ExceptionUtils.getRootCause(ex).getMessage
-            logger.error(s"Error during parsing. This should not happen. Answer with default NACK. Cause: $rootCause")
-            //TODO Find a sensible default format
-            val nack = "NACK"
-            encodeMllp(nack)
+          try {
+            logger.info("About to parse message: " + printableShort(each))
+            val message = parser.parse(each)
+            logger.info("Successfully parsed")
+
+            val ack = parser.encode(message.generateACK())
+            encodeMllp(ack)
+          } catch {
+            case ex: HL7Exception =>
+              val rootCause = printable(ExceptionUtils.getRootCause(ex).getMessage)
+              logger.error(s"Error during parsing. Problem with message structure. Answer with NACK. Cause: $rootCause")
+              //TODO Build NACK from message. Note that there is a piggyback ex.getResponseMessage which could be used
+              val nack = "NACK"
+              encodeMllp(nack)
+            case ex: Throwable =>
+              val rootCause = ExceptionUtils.getRootCause(ex).getMessage
+              logger.error(s"Error during parsing. This should not happen. Answer with default NACK. Cause: $rootCause")
+              //TODO Find a sensible default format
+              val nack = "NACK"
+              encodeMllp(nack)
+          }
         }
-      })
+        case _@Right(_) => encodeMllp("NACK")
+      }
 
 
     val handler = Sink.foreach[Tcp.IncomingConnection] { connection =>
       val serverEchoFlow = Flow[ByteString]
-        .wireTap(_ => logger.debug(s"Got message from client: ${connection.remoteAddress}"))
+        .wireTap(_ => logger.info(s"Got message from client: ${connection.remoteAddress}"))
         .wireTap(each => logger.debug("Size before framing: " + each.size))
         // frame input stream up to MLLP terminator: "END_OF_BLOCK + CARRIAGE_RETURN"
         .via(Framing.delimiter(
@@ -168,7 +185,7 @@ object Hl7MllpListenerAkkaStreams extends App {
   }
 
 
-  def localClient(id: Int, numberOfMesssages: Int, system: ActorSystem, address: String, port: Int): Unit = {
+  def localStreamingMessageClient(id: Int, numberOfMesssages: Int, system: ActorSystem, address: String, port: Int): Unit = {
     implicit val sys = system
     implicit val ec = system.dispatcher
 
@@ -178,6 +195,23 @@ object Hl7MllpListenerAkkaStreams extends App {
     val source = Source(hl7MllpMessages).throttle(10, 1.second).via(connection)
     val closed = source.runForeach(each => logger.info(s"Client: $id received echo: ${printable(each.utf8String)}"))
     closed.onComplete(each => logger.info(s"Client: $id closed: $each"))
+  }
+
+  //TODO Add retry meccano, when NACK is received
+  def localSingleMessageClient(id: Int, numberOfMesssages: Int, system: ActorSystem, address: String, port: Int): Unit = {
+    implicit val sys = system
+    implicit val ec = system.dispatcher
+
+    val connection = Tcp().outgoingConnection(address, port)
+
+    Source(1 to numberOfMesssages)
+      .throttle(1, 1.second)
+      .mapAsync(1){ each =>
+      val source = Source.single(ByteString(encodeMllp(generateTestMessage(each.toString)))).via(connection)
+      val closed = source.runForeach(each => logger.info(s"Client: $id received echo: ${printable(each.utf8String)}"))
+      closed.onComplete(each => logger.info(s"Client: $id closed: $each"))
+      Future(each)
+    }.runWith(Sink.ignore)
   }
 
   private def generateTestMessage(senderTraceID: String) = {
@@ -214,4 +248,13 @@ object Hl7MllpListenerAkkaStreams extends App {
     producer.send(new ProducerRecord(topic, partition0, null: String, InitialMsg))
     producer
   }
+
+  def initializeAdminClient(bootstrapServers: String) = {
+    val prop = new Properties()
+    prop.setProperty("bootstrap.servers", bootstrapServers)
+    AdminClient.create(prop)
+  }
 }
+
+case class Valid[T](payload: T)
+case class Invalid[T](payload: T, cause: Option[Throwable])
