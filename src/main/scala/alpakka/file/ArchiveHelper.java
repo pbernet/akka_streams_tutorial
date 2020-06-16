3680c67819c7de117cafec21bc9626ede0170cb3
pbernet <paul.bernet@elca.ch>
2020-06-16 18:39:14 +0200

    Add Archive functionality

---
+++ src/main/scala/alpakka/file/ArchiveHelper.java
@@ -0,0 +1,42 @@
+package alpakka.file;
+
+import akka.util.ByteString;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+/**
+ * Inspired by:
+ * [[docs.javadsl.ArchiveHelper]]
+ */
+public class ArchiveHelper {
+
+	public Map<String, ByteString> unzip(ByteString zipArchive) throws Exception {
+		ZipInputStream zis = new ZipInputStream(new ByteArrayInputStream(zipArchive.toArray()));
+		ZipEntry entry;
+		Map<String, ByteString> result = new HashMap<>();
+		try {
+			while ((entry = zis.getNextEntry()) != null) {
+				int count;
+				byte[] data = new byte[1024];
+
+				ByteArrayOutputStream dest = new ByteArrayOutputStream();
+				while ((count = zis.read(data, 0, 1024)) != -1) {
+					dest.write(data, 0, count);
+				}
+				dest.flush();
+				dest.close();
+				zis.closeEntry();
+				result.putIfAbsent(entry.getName(), ByteString.fromArray(dest.toByteArray()));
+			}
+		} finally {
+			zis.close();
+		}
+		return result;
+	}
+}
+
---
+++ src/main/scala/alpakka/file/ZipCryptoEcho.scala
@@ -0,0 +1,191 @@
+package alpakka.file
+
+import java.nio.file.Paths
+import java.security._
+import java.security.spec.{PKCS8EncodedKeySpec, X509EncodedKeySpec}
+
+import akka.actor.ActorSystem
+import akka.stream._
+import akka.stream.alpakka.file.ArchiveMetadata
+import akka.stream.alpakka.file.scaladsl.Archive
+import akka.stream.scaladsl._
+import akka.stream.stage._
+import akka.util.ByteString
+import javax.crypto._
+import javax.crypto.spec.{IvParameterSpec, SecretKeySpec}
+
+import scala.collection.JavaConverters._
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.util.{Failure, Success}
+
+/**
+  * File echo flow with Zip Archive feature and AES 128 encryption/decryption:
+  * 
+  * testfile.jpg (2) -> Archive -> AES 128 encryption -> testfile.encrypted 
+  * -> AES 128 decryption -> testfile_decrypted.zip -> Unarchive (ArchiveHelper.unzip)
+  * -> echo_testfile(1/2).jpg
+  *
+  * Make sure to run with a recent openjdk or with graalvm
+  *
+  * Inspired by:
+  * https://doc.akka.io/docs/alpakka/current/file.html#zip-archive
+  * https://gist.github.com/TimothyKlim/ec5889aa23400529fd5e
+  *
+  */
+
+private[this] class AesStage(cipher: Cipher) extends GraphStage[FlowShape[ByteString, ByteString]] {
+  val in = Inlet[ByteString]("in")
+  val out = Outlet[ByteString]("out")
+
+  override val shape = FlowShape.of(in, out)
+
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+    new GraphStageLogic(shape) {
+      setHandler(in, new InHandler {
+        override def onPush(): Unit = {
+          val bs = grab(in)
+          if (bs.isEmpty) push(out, bs)
+          else push(out, ByteString(cipher.update(bs.toArray)))
+        }
+
+        override def onUpstreamFinish(): Unit = {
+          val bs = ByteString(cipher.doFinal())
+          if (bs.nonEmpty) emit(out, bs)
+          complete(out)
+        }
+      })
+
+      setHandler(out, new OutHandler {
+        override def onPull(): Unit = {
+          pull(in)
+        }
+      })
+    }
+}
+
+object ZipCryptoEcho extends App {
+  implicit val system = ActorSystem("ZipCryptoEcho")
+  implicit val executionContext = system.dispatcher
+
+  val aesKeySize = 256
+  val aesKey = generateAesKey()
+  val initialisationVector = generateIv()
+
+  val sourceFileName = "./src/main/resources/testfile.jpg"
+  val encFileName = "testfile.encrypted"
+  val resultFileName = "testfile_decrypted.zip"
+
+  val fileStream1 = FileIO.fromPath(Paths.get(sourceFileName))
+  val fileStream2 = FileIO.fromPath(Paths.get(sourceFileName))
+
+  val filesStream = Source(
+    List(
+      (ArchiveMetadata("testfile1.jpg"), fileStream1),
+      (ArchiveMetadata("testfile2.jpg"), fileStream2)
+    )
+  )
+
+  val sinkEnc = FileIO.toPath(Paths.get(encFileName))
+  val sinkDec = FileIO.toPath(Paths.get(resultFileName))
+
+  val sourceZipped = filesStream.via(Archive.zip())
+  val sourceEnc = encryptAes(sourceZipped, aesKey, initialisationVector)
+  val doneEnc = sourceEnc.runWith(sinkEnc)
+
+  doneEnc.onComplete {
+    case Success(_) =>
+      val doneDec = decryptAes(sourceEnc, aesKey, initialisationVector).runWith(sinkDec)
+      doneDec.onComplete {
+        case Success(_) =>
+          val resultFileContentFut =
+            FileIO.fromPath(Paths.get(resultFileName)).runWith(Sink.fold(ByteString.empty)(_ ++ _))
+          val resultFileContent = Await.result(resultFileContentFut, 10.seconds)
+          val unzipResultMap = new ArchiveHelper().unzip(resultFileContent).asScala
+          unzipResultMap.foreach(each => {
+            println(s"Unzipped file: ${each._1}")
+            Source
+              .single(each._2)
+              .runWith(FileIO.toPath(Paths.get(s"echo_${each._1}")))
+          })
+          println(s"Saved ${unzipResultMap.size} echo .jpg files")
+        case Failure(ex) => println(s"Exception: $ex")
+      }
+      terminateWhen(doneDec)
+    case Failure(ex) => println(s"Exception: $ex")
+  }
+
+  def terminateWhen(done: Future[IOResult]) = {
+    done.onComplete {
+      case Success(_) =>
+        println(s"Flow Success. About to terminate...")
+        system.terminate()
+      case Failure(e) =>
+        println(s"Flow Failure: $e. About to terminate...")
+        system.terminate()
+    }
+  }
+
+  def generateAesKey() = {
+    val gen = KeyGenerator.getInstance("AES")
+    gen.init(aesKeySize)
+    val key = gen.generateKey()
+    val aesKey = key.getEncoded
+    aesKeySpec(aesKey)
+  }
+
+  def aesKeySpec(key: Array[Byte]) =
+    new SecretKeySpec(key, "AES")
+
+  def generateIv() = new SecureRandom().generateSeed(16)
+
+  private def aesCipher(mode: Int, keySpec: SecretKeySpec, ivBytes: Array[Byte]) = {
+    val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+    val ivSpec = new IvParameterSpec(ivBytes)
+    cipher.init(mode, keySpec, ivSpec)
+    cipher
+  }
+
+  def encryptAes(
+                  source: Source[ByteString, Any],
+                  keySpec: SecretKeySpec,
+                  ivBytes: Array[Byte]
+                ): Source[ByteString, Any] = {
+    val cipher = aesCipher(Cipher.ENCRYPT_MODE, keySpec, ivBytes)
+    source.via(new AesStage(cipher))
+  }
+
+  def decryptAes(
+                  source: Source[ByteString, Any],
+                  keySpec: SecretKeySpec,
+                  ivBytes: Array[Byte]
+                ): Source[ByteString, Any] = {
+    val cipher = aesCipher(Cipher.DECRYPT_MODE, keySpec, ivBytes)
+    source.via(new AesStage(cipher))
+  }
+
+  def getRsaKeyFactory() =
+    KeyFactory.getInstance("RSA")
+
+  def loadRsaPrivateKey(key: Array[Byte]) = {
+    val spec = new PKCS8EncodedKeySpec(key)
+    getRsaKeyFactory().generatePrivate(spec)
+  }
+
+  def loadRsaPublicKey(key: Array[Byte]) = {
+    val spec = new X509EncodedKeySpec(key)
+    getRsaKeyFactory().generatePublic(spec)
+  }
+
+  private def rsaCipher(mode: Int, key: Key) = {
+    val cipher = Cipher.getInstance("RSA")
+    cipher.init(mode, key)
+    cipher
+  }
+
+  def encryptRsa(bytes: Array[Byte], key: PublicKey): Array[Byte] =
+    rsaCipher(Cipher.ENCRYPT_MODE, key).doFinal(bytes)
+
+  def decryptRsa(bytes: Array[Byte], key: PrivateKey): Array[Byte] =
+    rsaCipher(Cipher.DECRYPT_MODE, key).doFinal(bytes)
+}
