1de7df0483b59a858f9313421d9553cf513634dd
pbernet <paul.bernet@gmail.com>
2020-09-06 11:14:46 +0200

    Resolve readResult completion

--- src/main/scala/alpakka/amqp/AmqpEcho.scala
+++ src/main/scala/alpakka/amqp/AmqpEcho.scala
@@ -9,8 +9,8 @@ import akka.{Done, NotUsed}
 import org.slf4j.{Logger, LoggerFactory}
 import org.testcontainers.containers.RabbitMQContainer
 
+import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
-import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.util.{Failure, Success}
 
 /**
@@ -18,7 +18,6 @@ import scala.util.{Failure, Success}
   * https://doc.akka.io/docs/alpakka/current/amqp.html
   *
   * TODO
-  * Resolve readResult completion
   * Parse readResults for content _.bytes.utf8String
   * Send to different queues
   *
@@ -26,8 +25,8 @@ import scala.util.{Failure, Success}
   */
 object AmqpEcho extends App {
   val logger: Logger = LoggerFactory.getLogger(this.getClass)
-  val systemClient1 = ActorSystem("AmqpEchoClient1")
-  val systemClient2 = ActorSystem("AmqpEchoClient2")
+  implicit val system = ActorSystem("AmqpEcho")
+  implicit val executionContext = system.dispatcher
 
   val (host, port) = ("127.0.0.1", 5672)
   val queueName = "myQueue"
@@ -36,17 +35,26 @@ object AmqpEcho extends App {
   rabbitMQContainer.start()
   logger.info(s"Started rabbitmq on: ${rabbitMQContainer.getContainerIpAddress}:${rabbitMQContainer.getMappedPort(port)}")
 
-  (1 to 2).par.foreach(each => clientSenderReceiver(each, systemClient1, rabbitMQContainer))
+  (1 to 1).par.foreach(each => roundTripSendReceive(each, rabbitMQContainer))
 
-  def clientSenderReceiver(id: Int, system: ActorSystem, rabbitMQContainer: RabbitMQContainer): Unit = {
-    implicit val sys = system
-    implicit val ec: ExecutionContextExecutor = system.dispatcher
+  def roundTripSendReceive(id: Int, rabbitMQContainer: RabbitMQContainer): Unit = {
 
     val mappedPort = rabbitMQContainer.getAmqpPort
     val amqpUri = s"amqp://$host:$mappedPort"
     val connectionProvider = AmqpCachedConnectionProvider(AmqpUriConnectionProvider(amqpUri))
     val queueDeclaration = QueueDeclaration(queueName)
 
+    send(id, connectionProvider, queueDeclaration)
+      .onComplete {
+        case Success(writeResult) =>
+          val noOfSentMsg = writeResult.seq.size
+          logger.info(s"Client: $id successfully sent: $noOfSentMsg messages to queue: $queueName. Starting receiver...")
+          receive(id, connectionProvider, queueDeclaration, noOfSentMsg)
+        case Failure(exception) => logger.info(s"Exception: $exception")
+      }
+  }
+
+  private def send(id: Int, connectionProvider: AmqpCachedConnectionProvider, queueDeclaration: QueueDeclaration) = {
     val settings = AmqpWriteSettings(connectionProvider)
       .withRoutingKey(queueName)
       .withDeclaration(queueDeclaration)
@@ -63,29 +71,26 @@ object AmqpEcho extends App {
         .via(amqpFlow)
         .wireTap(each => logger.info(s"WriteResult: $each"))
         .runWith(Sink.seq)
+    writeResult
+  }
 
-    writeResult.onComplete {
-      case Success(each) =>
-        logger.info(s"Successfully sent: ${each.seq.size} messages to queue: $queueName. Starting receiver...")
+  private def receive(id: Int, connectionProvider: AmqpCachedConnectionProvider, queueDeclaration: QueueDeclaration, noOfSentMsg: Int) = {
+    val amqpSource: Source[ReadResult, NotUsed] =
+      AmqpSource.atMostOnceSource(
+        NamedQueueSourceSettings(connectionProvider, queueName)
+          .withDeclaration(queueDeclaration)
+          .withAckRequired(false),
+        bufferSize = 10
+      )
 
-        val amqpSource: Source[ReadResult, NotUsed] =
-          AmqpSource.atMostOnceSource(
-            NamedQueueSourceSettings(connectionProvider, queueName)
-              .withDeclaration(queueDeclaration)
-              .withAckRequired(false),
-            bufferSize = 10
-          )
+    val readResult: Future[Seq[ReadResult]] =
+      amqpSource
+        .wireTap(each => logger.info(s"ReadResult: $each"))
+        .take(noOfSentMsg)
+        .runWith(Sink.seq)
 
-        val readResult: Future[Seq[ReadResult]] =
-          amqpSource
-            .wireTap(each => logger.info(s"ReadResult: $each"))
-            .runWith(Sink.seq)
-
-        //TODO Why is readResult not completed?
-        readResult.onComplete {
-          case Success(each) => logger.info(s"Successfully received: ${each.seq.size} messages from queue: $queueName")
-          case Failure(exception) => logger.info(s"Exception: $exception")
-        }
+    readResult.onComplete {
+      case Success(each) => logger.info(s"Client: $id successfully received: ${each.seq.size} messages from queue: $queueName")
       case Failure(exception) => logger.info(s"Exception: $exception")
     }
   }
