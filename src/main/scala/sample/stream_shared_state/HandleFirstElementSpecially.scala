720e43c4b12ae3827ab2fa5393570d468e609284
pbernet <paul.bernet@gmail.com>
2021-07-23 07:41:58 +0200

    Add more stateful examples

---
+++ src/main/scala/sample/stream_shared_state/HandleFirstElementSpecially.scala
@@ -0,0 +1,50 @@
+package sample.stream_shared_state
+
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
+
+/**
+  * Inspired by:
+  * https://stackoverflow.com/questions/40743047/handle-akka-streams-first-element-specially
+  * Doc:
+  * https://doc.akka.io/docs/akka/current/stream/operators/Source-or-Flow/prefixAndTail.html
+  *
+  * Similar to: [[DeferredStreamCreation]]
+  *
+  */
+object HandleFirstElementSpecially extends App {
+  val logger: Logger = LoggerFactory.getLogger(this.getClass)
+  implicit val system = ActorSystem("HandleFirstElementSpecially")
+  implicit val executionContext = system.dispatcher
+
+  val source = Source(List(1, 2, 3, 4, 5))
+  val first = Flow[Int].map(i => s"Processed first: $i")
+  val rest = Flow[Int].map(i => s"Processed rest: $i")
+
+  val printSink = Sink.foreach[String](each => println(s"Reached sink: $each"))
+
+  val done = source.prefixAndTail(1).flatMapConcat { case (head, tail) =>
+    // `head` is a Seq of prefix element(s), processed via `first` flow
+    // `tail` is a Seq of tail elements, processed via `rest` flow
+    // process head and tail elements with separate flows and concat results
+    Source(head).via(first).concat(tail.via(rest))
+  }.runWith(printSink)
+
+  terminateWhen(done)
+
+  def terminateWhen(done: Future[_]) = {
+    done.onComplete {
+      case Success(_) =>
+        println("Flow Success. About to terminate...")
+        system.terminate()
+      case Failure(e) =>
+        println(s"Flow Failure: $e. About to terminate...")
+        system.terminate()
+    }
+  }
+}
