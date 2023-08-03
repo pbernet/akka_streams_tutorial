4552444c695a31b093241662419a0c4808672344
pbernet <paul.bernet@gmail.com>
2023-08-03 19:04:46 +0200

    Wait for log msg to detect slow Elasticsearch startup

--- src/test/scala/alpakka/firehose/FirehoseEchoIT.java
+++ src/test/scala/alpakka/firehose/FirehoseEchoIT.java
@@ -8,11 +8,13 @@ import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.Container.ExecResult;
 import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import java.io.IOException;
+import java.time.Duration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.*;
@@ -34,17 +36,14 @@ public class FirehoseEchoIT {
     @Container
     public static LocalStackContainer localStack = new LocalStackContainer(DockerImageName.parse("localstack/localstack"))
             .withServices(FIREHOSE, S3, KINESIS)
-            .withClasspathResourceMapping("/localstack/init_firehose.sh", "/etc/localstack/init/ready.d/init_firehose.sh", BindMode.READ_ONLY);
-    // TODO Find a way to check instead of Thread.sleep - does not work yet
-    //.waitingFor(Wait.forLogMessage(".*[MetadataCreateIndexService]*", 1));
-
+            // Make sure that init_firehose.sh is executable and has linux line separator (LF)
+            .withClasspathResourceMapping("/localstack/init_firehose.sh", "/etc/localstack/init/ready.d/init_firehose.sh", BindMode.READ_ONLY)
+            // When Elasticsearch is ready it spits out this line in the log. This takes up to 240 seconds on a 2012 vintage MacBook Pro...
+            .waitingFor(Wait.forLogMessage(".*Active license is now \\[BASIC\\]; Security is disabled.*", 1).withStartupTimeout(Duration.ofSeconds(240)));
 
     @BeforeAll
     public static void beforeAll() throws InterruptedException, IOException {
         LOGGER.info("LocalStack container started on host address: {}", localStack.getEndpoint());
-        // Worst case: 2012 vintage MacBook Pro...
-        LOGGER.info("Waiting 240 seconds for initial setup to complete...");
-        Thread.sleep(240000);
 
         ExecResult result = localStack.execInContainer("awslocal", "firehose", "list-delivery-streams");
         LOGGER.debug("Result exit code: {}", result.getExitCode());
@@ -72,7 +71,7 @@ public class FirehoseEchoIT {
         FirehoseEcho firehoseEcho = new FirehoseEcho(localStack.getEndpointOverride(FIREHOSE), localStack.getAccessKey(), localStack.getSecretKey(), localStack.getRegion());
         assertThat(firehoseEcho.run()).isEqualTo(10);
 
-        // For now: Activate to manually check Elasticsearch entries
+        // Comment out to manually check Elasticsearch entries
         //browserClient();
         //Thread.sleep(1500000);
     }
