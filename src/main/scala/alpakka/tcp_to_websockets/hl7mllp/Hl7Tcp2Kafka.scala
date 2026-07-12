b5af3932d63745936a63e1c1897b36b203ee2467
paul <paul.bernet@gmail.com>
2026-07-12 15:10:15 +0200

    Simplify

--- src/main/scala/alpakka/tcp_to_websockets/hl7mllp/Hl7Tcp2Kafka.scala
+++ src/main/scala/alpakka/tcp_to_websockets/hl7mllp/Hl7Tcp2Kafka.scala
@@ -4,41 +4,35 @@ import ca.uhn.hl7v2.validation.impl.ValidationContextFactory
 import ca.uhn.hl7v2.{AcknowledgmentCode, DefaultHapiContext, HL7Exception}
 import org.apache.commons.lang3.StringUtils
 import org.apache.commons.lang3.exception.ExceptionUtils
-import org.apache.kafka.clients.admin.AdminClient
-import org.apache.kafka.clients.producer.{Producer, ProducerRecord}
+import org.apache.kafka.clients.producer.{ProducerRecord, RecordMetadata}
 import org.apache.kafka.common.serialization.StringSerializer
 import org.apache.pekko.NotUsed
 import org.apache.pekko.actor.ActorSystem
 import org.apache.pekko.kafka.ProducerSettings
+import org.apache.pekko.kafka.scaladsl.SendProducer
 import org.apache.pekko.stream.scaladsl.{Flow, Framing, Keep, Sink, Tcp}
 import org.apache.pekko.stream.{ActorAttributes, Supervision}
 import org.apache.pekko.util.ByteString
 import org.slf4j.{Logger, LoggerFactory}
 
-import java.util.Properties
 import scala.concurrent.Future
 import scala.util.control.NonFatal
 import scala.util.{Failure, Success}
 
 /**
-  * PoC of an akka streams based HL7 MLLP listener with the same behaviour as [[Hl7MllpListener]]:
+  * PoC of a pekko streams based HL7 MLLP listener with the same behavior as [[Hl7MllpListener]]:
   *  - Receive HL7 messages over tcp
   *  - Frame according to MLLP protocol
   *  - Parse using HAPI parser (all validation switched off)
-  *  - Send to Kafka
-  *  - Reply to client with ACK or NACK (eg on connection problems)
+  *  - Send to Kafka topic
+  *  - Reply to client with ACK or NACK (e.g. on connection issues)
   *
-  * TODO Add HL7 over HTTP channel:
-  * https://hapifhir.github.io/hapi-hl7v2/hapi-hl7overhttp/index.html
-  *
-  * TODO Avoid loosing messages in kafkaProducer (when Kafka is down) in a more graceful fashion
-  * Switch to:
-  * https://doc.akka.io/docs/alpakka-kafka/current/producer.html#producer-as-a-flow
-  * when done:
-  * https://github.com/akka/alpakka-kafka/issues/1101
+  * Run Kafka e.g. [[KafkaServerEmbedded]]
+  * Run this class to start the server
+  * Run [[Hl7TcpClient]] to produce messages
   *
   */
-class Hl7Tcp2Kafka(mappedPortKafka: Int = 9092) extends MllpProtocol {
+class Hl7Tcp2Kafka(mappedPortKafka: Int = 29092) extends MllpProtocol {
   val logger: Logger = LoggerFactory.getLogger(this.getClass)
   implicit val system: ActorSystem = ActorSystem()
 
@@ -46,13 +40,13 @@ class Hl7Tcp2Kafka(mappedPortKafka: Int = 9092) extends MllpProtocol {
 
   val bootstrapServers = s"127.0.0.1:$mappedPortKafka"
   val topic = "hl7-input"
-  //initial msg in topic, required to create the topic before any consumer subscribes to it
+  // initial msg in topic, required to create the topic before any consumer subscribes to it
   val InitialMsg = "InitialMsg"
   val partition0 = 0
   val producerSettings: ProducerSettings[String, String] = ProducerSettings(system, new StringSerializer, new StringSerializer)
     .withBootstrapServers(bootstrapServers)
-  val producer: Producer[String, String] = initializeTopic(topic)
-  val adminClient: AdminClient = initializeAdminClient(bootstrapServers)
+  val producer: SendProducer[String, String] = SendProducer(producerSettings)
+  initializeTopic(topic).failed.foreach(error => logger.error(s"Failed to initialize Kafka topic $topic", error))
 
   val (address, port) = ("127.0.0.1", 6160)
   var serverBinding: Future[Tcp.ServerBinding] = _
@@ -63,18 +57,17 @@ class Hl7Tcp2Kafka(mappedPortKafka: Int = 9092) extends MllpProtocol {
   }
 
   def stop(): Future[Unit] = {
-    serverBinding.map { b =>
-      b.unbind().onComplete {
-        _ =>
-          logger.info("TCP server stopped, stopping producer flow...")
-          system.terminate()
+    serverBinding
+      .flatMap(_.unbind())
+      .flatMap { _ =>
+        logger.info("TCP server stopped, stopping Kafka producer...")
+        producer.close()
       }
-    }
+      .flatMap(_ => system.terminate())
+      .map(_ => ())
   }
 
-
   def server(address: String, port: Int): Future[Tcp.ServerBinding] = {
-
     val deciderFlow: Supervision.Decider = {
       case NonFatal(e) =>
         logger.info(s"Stream failed with: ${e.getMessage}, going to restart")
@@ -82,42 +75,35 @@ class Hl7Tcp2Kafka(mappedPortKafka: Int = 9092) extends MllpProtocol {
       case _ => Supervision.Stop
     }
 
-    val frameUpToMllpTerminator = {
-      Framing.delimiter(
-        ByteString(END_OF_BLOCK + CARRIAGE_RETURN),
-        maximumFrameLength = 2048,
-        allowTruncation = true)
-    }
+    val frameUpToMllpTerminator = Framing.delimiter(
+      ByteString(END_OF_BLOCK + CARRIAGE_RETURN),
+      maximumFrameLength = 2048,
+      allowTruncation = true)
 
     // Remove MLLP START_OF_BLOCK from this message,
     // because only END_OF_BLOCK + CARRIAGE_RETURN is removed by framing
     val scrubber = Flow[String]
-      .map(each => StringUtils.stripStart(each, START_OF_BLOCK))
+      .map(StringUtils.stripStart(_, START_OF_BLOCK))
 
-    val kafkaProducer: Flow[String, Either[Valid[String], Invalid[String]], NotUsed] = Flow[String]
-      .map(each => {
-        //Retry handling for Kafka producers is now built-in, see:
-        //https://doc.akka.io/docs/alpakka-kafka/current/errorhandling.html#failing-producer
-        //When using the built-in strategy, the buffers are filled when Kafka goes down and then messages are sent once Kafka recovers.
-        //However, for this HL7 use case handling errors on the MLLP protocol level (by replying with a NACK) might be
-        //a better strategy to give the HL7 sender a chance to re-send in-flight messages in case THIS client goes down as well.
-        val topicExists = adminClient.listTopics.names.get.contains(topic)
-        if (topicExists) {
-          producer.send(new ProducerRecord(topic, partition0, null: String, each))
-          logger.info(s"Successfully sent to Kafka: ${printableShort(each)}.")
-          Left(Valid(each))
-        } else {
-          logger.error(s"Topic $topic does not exist or there is no connection to Kafka broker. Sending NACK to client for element ${printableShort(each)}")
-          Right(Invalid(each, Some(new RuntimeException("Kafka connection problem. Sending NACK to client"))))
-        }
-      })
+    val kafkaProducer: Flow[String, Either[Valid, Invalid], NotUsed] = Flow[String]
+      .mapAsync(1) { each =>
+        producer
+          .send(new ProducerRecord(topic, partition0, null: String, each))
+          .map[Either[Valid, Invalid]] { _ =>
+            logger.info(s"Successfully sent to Kafka: ${printableShort(each)}.")
+            Left(Valid(each))
+          }
+          .recover {
+            case NonFatal(error) =>
+              logger.error(s"Failed to send to Kafka. Sending NACK to client for element ${printableShort(each)}", error)
+              Right(Invalid(each))
+          }
+      }
 
     // Parse the hairy HL7 message beast
-    val hl7Parser = Flow[Either[Valid[String], Invalid[String]]]
+    val hl7Parser = Flow[Either[Valid, Invalid]]
       .map {
-        case left@Left(_) =>
-          val each = left.value.payload
-
+        case Left(Valid(each)) =>
           try {
             logger.info("About to parse message: " + printableShort(each))
             val parser = getPipeParser(true)
@@ -135,13 +121,12 @@ class Hl7Tcp2Kafka(mappedPortKafka: Int = 9092) extends MllpProtocol {
               logger.error(s"Error during parsing. This should not happen. Answer with default NACK. Cause: $rootCause")
               encodeMllp(generateNACK(each))
           }
-        case _@Right(Invalid(each, _)) =>
+        case Right(Invalid(each)) =>
           encodeMllp(generateNACK(each))
       }
 
-
     val handler = Sink.foreach[Tcp.IncomingConnection] { connection =>
-      val serverEchoFlow = Flow[ByteString]
+      val responseFlow = Flow[ByteString]
         .wireTap(each => logger.debug(s"Got message from client: ${connection.remoteAddress}. Size before framing: " + each.size))
         .via(frameUpToMllpTerminator)
         .wireTap(each => logger.debug("Size after framing: " + each.size))
@@ -151,15 +136,20 @@ class Hl7Tcp2Kafka(mappedPortKafka: Int = 9092) extends MllpProtocol {
         .via(hl7Parser)
         .map(ByteString(_))
         .withAttributes(ActorAttributes.supervisionStrategy(deciderFlow))
-        .watchTermination()((_, done) => done.onComplete {
-          case Failure(err) => logger.error(s"Server flow failed: $err")
-          case _ => logger.debug(s"Server flow terminated for client: ${connection.remoteAddress}")
-        })
-      connection.handleWith(serverEchoFlow)
+        .watchTermination() { (_, done) =>
+          done.onComplete {
+            case Failure(err) => logger.error(s"Server flow failed: $err")
+            case _ => logger.debug(s"Server flow terminated for client: ${connection.remoteAddress}")
+          }
+        }
+      connection.handleWith(responseFlow)
     }
 
-    val connections = Tcp().bind(interface = address, port = port)
-    val binding = connections.watchTermination()(Keep.left).to(handler).run()
+    val binding = Tcp()
+      .bind(interface = address, port = port)
+      .watchTermination()(Keep.left)
+      .to(handler)
+      .run()
 
     binding.onComplete {
       case Success(b) =>
@@ -171,11 +161,8 @@ class Hl7Tcp2Kafka(mappedPortKafka: Int = 9092) extends MllpProtocol {
     binding
   }
 
-
-  private def generateNACK(each: String) = {
-    val nackString = getPipeParser().parse(each).generateACK(AcknowledgmentCode.AE, null).encode()
-    nackString
-  }
+  private def generateNACK(each: String): String =
+    getPipeParser().parse(each).generateACK(AcknowledgmentCode.AE, null).encode()
 
   private def getPipeParser(withValidation: Boolean = false) = {
     val context = new DefaultHapiContext
@@ -191,17 +178,8 @@ class Hl7Tcp2Kafka(mappedPortKafka: Int = 9092) extends MllpProtocol {
     context.getPipeParser
   }
 
-  def initializeTopic(topic: String): Producer[String, String] = {
-    val producer = producerSettings.createKafkaProducer()
+  def initializeTopic(topic: String): Future[RecordMetadata] =
     producer.send(new ProducerRecord(topic, partition0, null: String, InitialMsg))
-    producer
-  }
-
-  def initializeAdminClient(bootstrapServers: String): AdminClient = {
-    val prop = new Properties()
-    prop.setProperty("bootstrap.servers", bootstrapServers)
-    AdminClient.create(prop)
-  }
 
   sys.ShutdownHookThread {
     logger.info("Got control-c cmd from shell or SIGTERM, about to shutdown...")
@@ -213,11 +191,11 @@ object Hl7Tcp2Kafka extends App {
   val server = new Hl7Tcp2Kafka()
   server.run()
 
-  def apply(mappedPort: Int) = new Hl7Tcp2Kafka(mappedPort)
+  def apply(mappedPort: Int): Hl7Tcp2Kafka = new Hl7Tcp2Kafka(mappedPort)
 
-  def stop() = server.stop()
+  def stop(): Future[Unit] = server.stop()
 }
 
-case class Valid[T](payload: T)
+case class Valid(payload: String)
 
-case class Invalid[T](payload: T, cause: Option[Throwable])
+case class Invalid(payload: String)
--- src/main/scala/alpakka/tcp_to_websockets/hl7mllp/Hl7TcpClient.scala
+++ src/main/scala/alpakka/tcp_to_websockets/hl7mllp/Hl7TcpClient.scala
@@ -7,10 +7,13 @@ import org.apache.pekko.stream.scaladsl.{Flow, Sink, Source, Tcp}
 import org.apache.pekko.util.ByteString
 import org.slf4j.{Logger, LoggerFactory}
 
-import scala.collection.parallel.CollectionConverters.*
+import scala.collection.parallel.CollectionConverters.ImmutableIterableIsParallelizable
 import scala.concurrent.Future
 import scala.concurrent.duration.*
 
+/**
+  * Client to send HL7 msgs to [[Hl7Tcp2Kafka]]
+  */
 class Hl7TcpClient(numberOfMessages: Int = 100) extends MllpProtocol {
   val logger: Logger = LoggerFactory.getLogger(this.getClass)
   implicit val system: ActorSystem = ActorSystem()
@@ -20,70 +23,62 @@ class Hl7TcpClient(numberOfMessages: Int = 100) extends MllpProtocol {
   val (address, port) = ("127.0.0.1", 6160)
   val connection: Flow[ByteString, ByteString, Future[Tcp.OutgoingConnection]] = Tcp().outgoingConnection(address, port)
 
-  (1 to 1).par.foreach(each => localSingleMessageClient(each, numberOfMessages))
-  //(1 to 1).par.foreach(each => localStreamingMessageClient(each, 1000))
+  (1 to 2).par.foreach(each => localSingleMessageClient(each, numberOfMessages))
 
   def localSingleMessageClient(client: Int, nbrOfMgs: Int): Unit = {
     Source(1 to nbrOfMgs)
       .throttle(1, 1.second)
-      .mapAsync(1)(msgID => sendAndReceive(s"$client-$msgID", None))
+      .mapAsync(1)(msgID => sendAndReceive(s"$client-$msgID"))
       .runWith(Sink.ignore)
   }
 
   def localStreamingMessageClient(id: Int, nbrOfMgs: Int): Unit = {
-    val hl7MllpMessages = (1 to nbrOfMgs).map(each => ByteString(encodeMllp(generateTestMessage(each.toString))))
-    val source = Source(hl7MllpMessages).throttle(10, 1.second).via(connection)
-    val closed = source.runForeach(each => logger.info(s"Client: $id received echo: ${printable(each.utf8String)}"))
+    val messages = (1 to nbrOfMgs).map(each => ByteString(encodeMllp(generateTestMessage(each.toString))))
+    val closed = Source(messages)
+      .throttle(10, 1.second)
+      .via(connection)
+      .runForeach(each => logger.info(s"Client: $id received echo: ${printable(each.utf8String)}"))
     closed.onComplete(each => logger.info(s"Client: $id closed: $each"))
   }
 
-  private def sendAndReceive(traceID: String, hl7Msg: Option[String]): Future[NotUsed] = {
-    val hl7MsgString = hl7Msg.getOrElse(generateTestMessage(traceID))
-    val source = Source.single(ByteString(encodeMllp(hl7MsgString))).via(connection)
-    val closed = source.runForeach(each =>
-      if (isNACK(each)) {
-        logger.info(s"Client for traceID: $traceID received NACK: ${printable(each.utf8String)}")
+  private def sendAndReceive(traceID: String): Future[NotUsed] = {
+    val message = ByteString(encodeMllp(generateTestMessage(traceID)))
+    val closed = Source.single(message).via(connection).runForeach { response =>
+      if (isNACK(response)) {
+        logger.info(s"Client for traceID: $traceID received NACK: ${printable(response.utf8String)}")
         throw new RuntimeException("NACK")
       } else {
-        logger.info(s"Client for traceID: $traceID received ACK: ${printable(each.utf8String)}")
+        logger.info(s"Client for traceID: $traceID received ACK: ${printable(response.utf8String)}")
       }
-    ).recoverWith {
+    }.recoverWith {
       case ex: RuntimeException =>
         logger.warn(s"Client for traceID: $traceID about to retry, because of: $ex")
-        sendAndReceive(traceID, hl7Msg)
-      case e: Throwable => Future.failed(e)
+        sendAndReceive(traceID)
     }
     closed.onComplete(each => logger.debug(s"Client for traceID: $traceID closed: $each"))
     Future(NotUsed)
   }
 
-
-  private def generateTestMessage(senderTraceID: String) = {
-    //For now put the senderTraceID into the "sender lab" field to follow the messages across the workflow
-    val message = new StringBuilder
-    message ++= s"MSH|^~\\&|$senderTraceID|MCM|LABADT|MCM|198808181126|SECURITY|ADT^A01|1234|P|2.5.1|"
-    message ++= CARRIAGE_RETURN
-    message ++= "EVN|A01|198808181123||"
-    message ++= CARRIAGE_RETURN
-    message ++= "PID|||PATID1234^5^M11^ADT1^MR^MCM~123456789^^^USSSA^SS||EVERYMAN^ADAM^A^III||19610615|M||C|1200 N ELM STREET^^GREENSBORO^NC^27401-1020"
-    message ++= CARRIAGE_RETURN
-    message ++= "NK1|1|JONES^BARBARA^K|SPO^Spouse^HL70063|171 ZOBERLEIN^^ISHPEMING^MI^49849^|"
-    message ++= CARRIAGE_RETURN
-    message ++= "PV1|1|I|2000^2012^01||||004777^LEBAUER^SIDNEY^J.|||SUR||||9|A0|"
-    message ++= CARRIAGE_RETURN
-    message.toString()
+  private def generateTestMessage(senderTraceID: String): String = {
+    // For now put the senderTraceID into the "sender lab" field to follow the messages across the workflow
+    Seq(
+      s"MSH|^~\\&|$senderTraceID|MCM|LABADT|MCM|198808181126|SECURITY|ADT^A01|1234|P|2.5.1|",
+      "EVN|A01|198808181123||",
+      "PID|||PATID1234^5^M11^ADT1^MR^MCM~123456789^^^USSSA^SS||EVERYMAN^ADAM^A^III||19610615|M||C|1200 N ELM STREET^^GREENSBORO^NC^27401-1020",
+      "NK1|1|JONES^BARBARA^K|SPO^Spouse^HL70063|171 ZOBERLEIN^^ISHPEMING^MI^49849^|",
+      "PV1|1|I|2000^2012^01||||004777^LEBAUER^SIDNEY^J.|||SUR||||9|A0|")
+      .mkString("", CARRIAGE_RETURN, CARRIAGE_RETURN)
   }
 
   private def isNACK(message: ByteString): Boolean = {
-    message.utf8String.contains(AcknowledgmentCode.AE.name()) ||
-      message.utf8String.contains(AcknowledgmentCode.AR.name()) ||
-      message.utf8String.contains(AcknowledgmentCode.CE.name()) ||
-      message.utf8String.contains(AcknowledgmentCode.CR.name())
+    val response = message.utf8String
+    Seq(AcknowledgmentCode.AE, AcknowledgmentCode.AR, AcknowledgmentCode.CE, AcknowledgmentCode.CR)
+      .exists(code => response.contains(code.name()))
   }
 }
 
 object Hl7TcpClient extends App {
   val client = new Hl7TcpClient()
 
-  def apply(numberOfMessages: Int = 100) = new Hl7TcpClient(numberOfMessages)
+  def apply(numberOfMessages: Int = 100): Hl7TcpClient = new Hl7TcpClient(numberOfMessages)
 }
